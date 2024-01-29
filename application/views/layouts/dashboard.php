9ec27100b0c52476b74c0429b6b0c126c75a1f5d
rezaandreannn <andreanreza042@gmail.com>
2024-01-29 10:39:12 +0700

    install project dan admin lte

---
+++ application/views/layouts/dashboard.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EMR | Dashboard</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/fontawesome-free/css/all.min.css'); ?>">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'); ?>">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css'); ?>">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/jqvmap/jqvmap.min.css'); ?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/dist/css/adminlte.min.css'); ?>">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css'); ?>">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/daterangepicker/daterangepicker.css'); ?>">
+    <!-- summernote -->
+    <link rel="stylesheet" href="<?= base_url('assets/AdminLTE/plugins/summernote/summernote-bs4.min.css'); ?>">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- Preloader -->
+        <?php $this->load->view('partials/dashboard/_loader'); ?>
+        <!-- Preloader -->
+
+        <!-- Navbar -->
+        <?php $this->load->view('partials/dashboard/_navbar'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php $this->load->view("partials/dashboard/_sidebar"); ?>
+        <!-- Main Sidebar Container -->
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <?php $this->load->view('partials/dashboard/_content_header'); ?>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <?php $this->load->view($content) ?>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        <!-- footer -->
+        <?php $this->load->view('partials/dashboard/_footer'); ?>
+        <!-- footer -->
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/jquery/jquery.min.js'); ?>"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/jquery-ui/jquery-ui.min.js'); ?>"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+    <!-- ChartJS -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/chart.js/Chart.min.js'); ?>"></script>
+    <!-- Sparkline -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/sparklines/sparkline.js'); ?>"></script>
+    <!-- JQVMap -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/jqvmap/jquery.vmap.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/AdminLTE/plugins/jqvmap/maps/jquery.vmap.usa.js'); ?>"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/jquery-knob/jquery.knob.min.js'); ?>"></script>
+    <!-- daterangepicker -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/moment/moment.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/AdminLTE/plugins/daterangepicker/daterangepicker.js'); ?>"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'); ?>"></script>
+    <!-- Summernote -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/summernote/summernote-bs4.min.js'); ?>"></script>
+    <!-- overlayScrollbars -->
+    <script src="<?= base_url('assets/AdminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'); ?>"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url('assets/AdminLTE/dist/js/adminlte.js'); ?>"></script>
+    <!-- AdminLTE for demo purposes -->
+    <!-- <script src="<?= base_url('assets/AdminLTE/dist/js/demo.js'); ?>"></script> -->
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="<?= base_url('assets/AdminLTE/dist/js/pages/dashboard.js'); ?>"></script>
+</body>
+
+</html>
